df437c3fed0e5b16c59d01ee1a9dadec8b13c02d
joevallillo <joevallillo@yahoo.com>
2018-06-18 17:09:03 -0400

    Main functionality in place
    Main functionality in place

--- comic_service.py
+++ comic_service.py
@@ -3,6 +3,7 @@ import collections
 import bs4
 import os
 import shutil
+import img2pdf
 
 ComicBook = collections.namedtuple('ComicBook',
                                    'name url')
@@ -33,15 +34,17 @@ def get_comics_from_html(html):
 
     return comics
 
-# TODO: Fix folder/file naming
+# TODO: Fix folder/file naming, handle multiple pages
 def get_comic(comic, folder):
     url = comic.url
     html = get_html_from_web(url + '/full')
     page_urls = get_pages_from_html(html)
+    page_num = 0
     for page_url in page_urls:
+        page_num += 1
         page_data = get_page_data_from_web(page_url)
-        save_image(folder, comic.name, page_data)
-    make_pdf_from_images(folder)
+        save_image(folder, str(page_num), page_data)
+    #make_pdf_from_images(folder)
 
 
 def get_pages_from_html(html):
@@ -59,13 +62,20 @@ def get_pages_from_html(html):
 
 
 def get_page_data_from_web(url):
-    return requests.get(url, stream=True)
+    return requests.get(url, stream=True).raw
 
 # TODO: Need to implement
 def make_pdf_from_images(folder):
-    pass
+    file = os.path.basename(folder) + '.pdf'
+    print(file)
+    with open(file, 'wb') as fout:
+        fout.write(img2pdf.convert([i for i in os.listdir(folder) if i.endswith('.jpg')]))
 
 def save_image(folder, name, data):
     filename = os.path.join(folder, name + '.jpg')
     with open(filename, 'wb') as fout:
         shutil.copyfileobj(data, fout)
+
+
+if __name__ == '__main__':
+    make_pdf_from_images(r"C:\Users\Joseph\Documents\python\ComicPy\comics\carnage 2016\Carnage (2016) #1")
--- program.py
+++ program.py
@@ -3,13 +3,12 @@ import os
 
 
 def main():
-    # get output location
-    folder = get_or_create_output_folder()
-
     # get user input for what series they want to download
-    #series = input("")
-    series = "weapon x 2017"
+    series = input("Enter the name of the series you would like to download")
     url = comic_service.url_for_series(series)
+
+    # get output location
+    folder = get_or_create_output_folder(series_folder=series)
 
     # download main page from web
     html = comic_service.get_html_from_web(url)
@@ -18,13 +17,26 @@ def main():
     comics = comic_service.get_comics_from_html(html)
 
     for comic in comics:
-        # TODO: Determine if we have the comic first and skip if we do
-        comic_service.get_comic(comic, folder)
+        comic_folder = get_or_create_output_folder(series_folder=series, issue_folder=comic.name)
+        # TODO: Skip existing comicsS
+        comic_service.get_comic(comic, comic_folder)
 
 
+def get_or_create_output_folder(series_folder=None, issue_folder=None):
+    base_folder = os.path.abspath(os.path.dirname(__file__))
+    folder = 'comics'
+    full_path = os.path.join(base_folder, folder)
 
-def get_or_create_output_folder():
-    return r"C:\users\jvallillo\desktop\test"
+    if series_folder:
+        full_path = os.path.join(full_path, series_folder)
+        if issue_folder:
+            full_path = os.path.join(full_path, issue_folder)
+
+    if not os.path.exists(full_path) or not os.path.isdir(full_path):
+        print('Creating new directory at {}'.format(full_path))
+        os.mkdir(full_path)
+
+    return full_path
 
 
 if __name__ == '__main__':
